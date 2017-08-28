ec9da3f67c62692ac38639d1bac80b935f4a2368
53k41iga <sonlaneedsme@outlook.com>
2017-08-28 17:57:24 +0700

    Info of game

---
+++ info-game.h
@@ -0,0 +1,21 @@
+#ifndef INFO_GAME_H_
+	#define INFO_GAME_H_
+
+
+//Define board as board[y][x]
+
+#define HEIGHT 16
+#define WIDTH 16
+
+#define X_MARK 1
+#define O_MARK 0
+#define NONE_MARK -1
+
+#define TRUE 1
+#define FALSE 0
+
+#define NUM_PRESENT 26
+
+const char PRESENT_ALPHABET[NUM_PRESENT] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+#endif
