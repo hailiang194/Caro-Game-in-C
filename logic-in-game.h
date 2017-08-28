08019487a27295aa2d33d9c38fb1fa93b152148a
53k41iga <sonlaneedsme@outlook.com>
2017-08-28 18:37:47 +0700

    Update true place

--- logic-in-game.h
+++ logic-in-game.h
@@ -4,12 +4,22 @@
 #include "info-game.h"
 
 int is_out_of_board(int x, int y);
-int can_place(int x, int y);
-int is_true_mark_place(int x, int y);
+int can_place(const int board[HEIGHT][WIDTH] ,int x, int y);
+int is_true_mark_place(const int board[HEIGHT][WIDTH] ,int x, int y);
 
 int is_out_of_board(int x, int y)
 {
 	return ((x < 0 || x > WIDTH) || (y < 0 || y > HEIGHT));
+}
+
+int can_place(const int board[HEIGHT][WIDTH],int x, int y)
+{
+	return (board[x][y] == NONE_MARK);
+}
+
+int is_true_mark_place(const int board[HEIGHT][WIDTH], int x, int y)
+{
+	return (!is_out_of_board(x, y) && can_place(board, x, y));
 }
 
 #endif
