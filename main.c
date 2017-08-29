4e09383875e2500fe3aa669cfd57124e49032473
53k41iga <sonlaneedsme@outlook.com>
2017-08-29 16:53:28 +0700

    Update show_game_scene

--- main.c
+++ main.c
@@ -4,14 +4,24 @@
 #include "info-game.h"
 #include "process-in-game.h"
 
+void show_game_scene(int board[HEIGHT][WIDTH], int *isX, int *lastX, int *lastY);
 
 int main()
 {
 	int board[HEIGHT][WIDTH] = {0};
 	int *isX = (int *)malloc(sizeof(int));
 
-	setup(board, isX);
+	int *lastX = (int *)malloc(sizeof(int));
+	int *lastY = (int *)malloc(sizeof(int));
 
-	game_scence(board);
+	setup(board, isX, lastX, lastY);
+
+	show_game_scene(board, isX, lastX, lastY);
 	return 0;
+}
+
+void show_game_scene(int board[HEIGHT][WIDTH], int *isX, int *lastX, int *lastY)
+{
+	while(!is_game_over(board, *lastX, *lastY))
+		update(board, isX, lastX, lastY);
 }
