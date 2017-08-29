49961c0ce229d398fbdda069e7b272a4836c226b
53k41iga <sonlaneedsme@outlook.com>
2017-08-29 16:52:06 +0700

    Update is_game_over

--- logic-in-game.h
+++ logic-in-game.h
@@ -6,6 +6,8 @@
 int is_out_of_board(int x, int y);
 int can_place(const int board[HEIGHT][WIDTH] ,int x, int y);
 int is_true_mark_place(const int board[HEIGHT][WIDTH] ,int x, int y);
+int have_no_none_mark(const int board[HEIGHT][WIDTH]);
+int is_game_over(const int board[HEIGHT][WIDTH], int lastX, int lastY);
 
 int is_out_of_board(int x, int y)
 {
@@ -20,6 +22,22 @@ int can_place(const int board[HEIGHT][WIDTH],int x, int y)
 int is_true_mark_place(const int board[HEIGHT][WIDTH], int x, int y)
 {
 	return (!is_out_of_board(x, y) && can_place(board, x, y));
+}
+
+int have_no_none_mark(const int board[HEIGHT][WIDTH])
+{
+	for(int i = 0; i < HEIGHT; i++)
+		for(int j = 0; j < WIDTH; j++)
+			if(board[i][j] == NONE_MARK)
+				return FALSE;
+
+	//there is no none marks
+	return TRUE;
+}
+
+int is_game_over(const int board[HEIGHT][WIDTH], int lastX, int lastY)
+{
+	return have_no_none_mark(board);
 }
 
 #endif
