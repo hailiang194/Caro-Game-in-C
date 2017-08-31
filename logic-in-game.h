43bb8adb6fc89d3c7e6515a8f84e4bbc4aa1ebf3
53k41iga <sonlaneedsme@outlook.com>
2017-08-31 15:05:02 +0700

    Remove is_out_of_board and can_place

--- logic-in-game.h
+++ logic-in-game.h
@@ -2,22 +2,12 @@
 	#define LOGIC_IN_GAME_H_
 
 #include "info-game.h"
+#include "check-overline.h"
 
-int is_out_of_board(int x, int y);
-int can_place(const int board[HEIGHT][WIDTH] ,int x, int y);
 int is_true_mark_place(const int board[HEIGHT][WIDTH] ,int x, int y);
 int have_no_none_mark(const int board[HEIGHT][WIDTH]);
 int is_game_over(const int board[HEIGHT][WIDTH], int lastX, int lastY);
 
-int is_out_of_board(int x, int y)
-{
-	return ((x < 0 || x > WIDTH) || (y < 0 || y > HEIGHT));
-}
-
-int can_place(const int board[HEIGHT][WIDTH],int x, int y)
-{
-	return (board[x][y] == NONE_MARK);
-}
 
 int is_true_mark_place(const int board[HEIGHT][WIDTH], int x, int y)
 {
@@ -37,7 +27,7 @@ int have_no_none_mark(const int board[HEIGHT][WIDTH])
 
 int is_game_over(const int board[HEIGHT][WIDTH], int lastX, int lastY)
 {
-	return have_no_none_mark(board);
+	return (have_no_none_mark(board) || have_winner(board, lastX, lastY));
 }
 
 #endif
